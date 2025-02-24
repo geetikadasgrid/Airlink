05ce2fd686955ffe48d42a8958c89f10338834da
Geetika <gdas@griddyamics.com>
2025-02-25 01:38:08 +0530

    Added host config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: true,
+    proxy: {
+      "/api": {
+        target: "https://api.api-ninjas.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
